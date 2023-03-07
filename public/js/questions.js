40fa3e32206424af81ba3805f5d2e59ac03c33a6
shetty2112 <shettykshitij2112@gmail.com>
2023-03-08 00:17:26 +0530

    first version

---
+++ public/js/questions.js
@@ -0,0 +1,84 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Do you Have Regular Headache?",
+    answer: "Yes" || "No",
+    options: [
+      "Yes",
+      "No"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Do You Have Seizures Sometimes?",
+    options: [
+      "Yes",
+      "No"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Do You Have Difficulty In Thinking Or Understanding?",
+    options: [
+      "Yes",
+      "No"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Do You Have Weakness In Any Part Of Your Body?",
+    options: [
+      "Yes",
+      "No"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Do You Have Paralysis Before?",
+    options: [
+      "Yes",
+      "No"
+    ]
+  },
+  
+  {
+    numb: 6,
+    question: "Do You Have Dizziness?",
+    options: [
+      "Yes",
+      "No"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Do You Have Vision Problem?",
+    options: [
+      "Yes",
+      "No"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Do You Have Hearing Issue?",
+    options: [
+      "Yes",
+      "No"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Are Your Reflexes Normal?",
+    options: [
+      "Yes",
+      "No"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Do You Sometimes Loose Your Balance While Standing?",
+    options: [
+      "Yes",
+      "No"
+    ]
+  }
+];
---
+++ public/js/script.js
@@ -0,0 +1,141 @@
+ const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box");
+const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = info_box.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz_box");
+const result_box = document.querySelector(".result_box");
+const option_list = document.querySelector(".option_list");
+const time_line = document.querySelector("header .time_line");
+const timeText = document.querySelector(".timer .time_left_txt");
+const timeCount = document.querySelector(".timer .timer_sec");
+const res = document.querySelector("resc");
+
+start_btn.onclick = ()=>{
+    info_box.classList.add("activeInfo"); 
+}
+
+exit_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo"); 
+}
+
+continue_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo");
+    quiz_box.classList.add("activeQuiz"); 
+    showQuetions(0); 
+    queCounter(1);  
+}
+
+let que_count = 0;
+let que_numb = 1;
+let userScore = 0;
+let counter;
+let counterLine;
+let widthValue = 0;
+
+const restart_quiz = result_box.querySelector(".buttons .restart");
+const quit_quiz = result_box.querySelector(".buttons .quit");
+
+restart_quiz.onclick = ()=>{
+    quiz_box.classList.add("activeQuiz"); 
+    result_box.classList.remove("activeResult"); 
+    timeValue = 15; 
+    que_count = 0;
+    que_numb = 1;
+    userScore = 0;
+    widthValue = 0;
+    neg = 0;
+    pos = 0;
+    showQuetions(que_count); 
+    queCounter(que_numb); 
+    next_btn.classList.remove("show"); 
+}
+
+quit_quiz.onclick = ()=>{
+    window.location.reload(); 
+}
+
+const next_btn = document.querySelector("footer .next_btn");
+const bottom_ques_counter = document.querySelector("footer .total_que");
+
+next_btn.onclick = ()=>{
+    if(que_count < questions.length - 1){ 
+        que_count++; 
+        que_numb++; 
+        showQuetions(que_count); 
+        queCounter(que_numb); 
+        timeText.textContent = "Feel Free To Share With Us!!"; 
+        next_btn.classList.remove("show"); 
+    }else{
+        showResult();
+    }
+}
+
+function showQuetions(index){
+    const que_text = document.querySelector(".que_text");
+    let que_tag = '<span>'+ questions[index].numb + ". " + questions[index].question +'</span>';
+    let option_tag = '<div class="option"><span>'+ questions[index].options[0] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[1] +'</span></div>';
+    que_text.innerHTML = que_tag; 
+    option_list.innerHTML = option_tag;     
+    const option = option_list.querySelectorAll(".option");
+
+    for(i=0; i < option.length; i++){
+        option[i].setAttribute("onclick", "optionSelected(this)");
+    }
+}
+
+let tickIconTag = '<div class="icon tick"><i class="fas fa-check"></i></div>';
+let crossIconTag = '<div class="icon cross"><i class="fas fa-times"></i></div>';
+
+function optionSelected(answer){
+    let userAns = answer.textContent; 
+    let correcAns = questions[que_count].answer; 
+    const allOptions = option_list.children.length; 
+    
+    if(userAns == "Yes" || userAns == "No"){ 
+
+        if(userAns == "Yes")
+        {
+            userScore += 1; 
+        }
+        
+       
+        answer.classList.add("correct"); 
+        answer.insertAdjacentHTML("beforeend", tickIconTag); 
+        console.log("Correct Answer");
+        console.log("Your correct answers = " + userScore);
+    }
+    for(i=0; i < allOptions; i++){
+        option_list.children[i].classList.add("disabled"); 
+    }
+    next_btn.classList.add("show"); 
+}
+
+function showResult(){
+    info_box.classList.remove("activeInfo"); 
+    quiz_box.classList.remove("activeQuiz"); 
+    result_box.classList.add("activeResult"); 
+    const scoreText = result_box.querySelector(".score_text");
+    if (userScore >= 7){ 
+        res = document.getElementById("resc").innerHTML="You Need checkup😐!!";
+    }
+    else if(userScore < 7){ 
+        res = document.getElementById("resc").innerHTML="You Are Safe😀😍!!";
+    }
+    else{ 
+
+    }
+}
+
+function startTimerLine(time){
+    counterLine = setInterval(timer, 29);
+    function timer(){
+        time = 600;
+        time_line.style.width = time + "px";
+    }
+}
+
+function queCounter(index){
+    let totalQueCounTag = '<span><p>'+ index +'</p> of <p>'+ questions.length +'</p> Questions</span>';
+    bottom_ques_counter.innerHTML = totalQueCounTag; 
+}
